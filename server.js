870fbd8d4805272a34ed7f213a6ac66dc74dca36
tota12 <91630912+tota12@users.noreply.github.com>
2023-05-26 19:45:16 +0300

    first commit

---
+++ server.js
@@ -0,0 +1,59 @@
+// Express to run server and routes
+const express = require('express');
+const app = express();
+// file-upload for uploading files
+const upload = require('express-fileupload')
+const ebookConverter = require('node-ebook-converter');
+const path = require('path');
+// Cors for cross origin allowance
+const cors = require('cors');
+app.use(cors());
+/* Dependencies */
+const bodyParser = require('body-parser');
+/* Middleware*/
+app.use(upload());
+app.use(express.urlencoded({extended: false}));
+app.use(express.json());
+// Initialize the main project folder
+app.use(express.static('website'));
+// Spin up the server
+const port = 5000;
+app.listen(port, ()=>
+  console.log(`running on localhost: ${port}`));
+//requests
+app.get('/', (req, res) => {
+  res.sendFile(path.resolve('index.html'));
+  //res.sendFile(__dirname + '/index.html')
+})
+app.post('/', (req, res) => 
+{
+  if(req.files)
+  {
+    var file = req.files.file;
+    var fileName = file.name;
+    console.log(fileName);
+    file.mv('./input/' + fileName, function(error)
+    {
+      if(error)
+      {
+        console.log(error);
+      }else
+      {
+        console.log('File Uploaded');
+      }
+    })
+    ebookConverter.convert({
+      input: `input/${fileName}`,
+      output: `output/${fileName}.epub`,
+      delete: true
+    }).then(response => console.log(response))
+      .then(()=>{
+        var output = `${__dirname}\\output\\${fileName}.epub`;
+        res.sendFile(output);
+      })
+      .catch(error => console.error(error));
+    
+    
+  }
+})
+ebookConverter.setPoolSize(2);
